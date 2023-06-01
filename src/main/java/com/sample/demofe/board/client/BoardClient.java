708d5ebdae8dcbe49b59b6f76a5914e712b53749
ray <ray.lee@osckorea.com>
2023-06-01 13:16:46 +0900

    V 1.0.0

---
+++ src/main/java/com/sample/demofe/board/client/BoardClient.java
@@ -0,0 +1,25 @@
+package com.sample.demofe.board.client;
+
+import java.util.List;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+import com.sample.demofe.board.model.Board;
+
+@FeignClient(name = "BOARD-CLIENT", url = "http://board-api:8000/boards")
+public interface BoardClient {
+
+    @GetMapping
+    public List<Board> getAllBoards();
+
+    @GetMapping("/:{id}")
+    public Board getBoardById(@PathVariable Long id);
+
+    @PostMapping
+    public String createBoardPost(@RequestBody Board dto);
+
+    @PutMapping("/:{id}")
+    public String editBoardPost(@PathVariable Long id, @RequestBody Board dto);
+
+    @DeleteMapping("/:{id}")
+    public String deleteBoardPost(@PathVariable Long id);
+}
---
+++ src/test/java/com/sample/demofe/DemoFeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.demofe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
