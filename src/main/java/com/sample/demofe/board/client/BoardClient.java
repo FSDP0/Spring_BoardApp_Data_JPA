4f1914ad00f9de8d7e3bf728ef5b3aaede2fcdd0
ray <ray.lee@osckorea.com>
2023-06-07 01:12:34 +0900

    V 1.0.3

--- src/main/java/com/sample/demofe/board/client/BoardClient.java
+++ src/main/java/com/sample/demofe/board/client/BoardClient.java
@@ -2,11 +2,9 @@ package com.sample.demofe.board.client;
 
 import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.*;
 import com.sample.demofe.board.model.Board;
 
-@PropertySource("classpath:application.yml")
 @FeignClient(name = "BOARD-CLIENT", url = "${api.url}")
 public interface BoardClient {
 
@@ -24,4 +22,5 @@ public interface BoardClient {
 
     @DeleteMapping("/:{id}")
     public String deleteBoardPost(@PathVariable Long id);
+
 }
