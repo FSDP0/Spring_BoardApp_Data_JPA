753d7980586880396271408ab412f03703e24022
ray <ray.lee@osckorea.com>
2023-06-19 14:36:43 +0900

    V 1.0.9 : Build Test

--- src/main/java/com/sample/demoapi/DemoApiApplication.java
+++ src/main/java/com/sample/demoapi/DemoApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class DemoApiApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApiApplication.class, args);
 	}
