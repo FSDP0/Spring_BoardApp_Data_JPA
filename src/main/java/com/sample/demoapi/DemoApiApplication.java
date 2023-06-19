d86ec6981fee5d257c3ef607e334cc2eb991617c
ray <ray.lee@osckorea.com>
2023-06-19 14:38:16 +0900

    V 1.0.9 : Build Test

--- src/main/java/com/sample/demoapi/DemoApiApplication.java
+++ src/main/java/com/sample/demoapi/DemoApiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class DemoApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApiApplication.class, args);
 	}
